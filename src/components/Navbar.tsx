17870b947115e6cf17c8e2295129bcd8549254b6
TempUser <temp@mail>
2025-05-31 11:06:00 +0300

    Добавлены иконки

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
+import './Navbar.css'
+
+import Icons from '../static/Icons';
 
 export default function Navbar() {
 
-
+    const score = 102;
 
     return (
-        <div>
-            <h1 className="red">Navbar</h1>
+        <div className='navbar-box'>
+            <div className='score-box'>{score}{Icons.score}</div>
         </div>
     );
 }
---
+++ src/static/Icons.tsx
@@ -0,0 +1,11 @@
+
+import { SiBitcoin } from "react-icons/si";
+
+const Icons = {
+
+	score: <SiBitcoin/>, 
+
+};
+
+
+export default Icons;
