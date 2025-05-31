ac386271b62e0d586355b9f0b4eb43f9f811080e
TempUser <temp@mail>
2025-05-31 12:05:24 +0300

    улучшение стилей
    размер области видимости, цвета

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,13 +7,15 @@ export default function Home() {
 
 
     return (
-        <div>
+        <div className="body">
             <Navbar/>
             <div className="page-box">
+                <h1>Цель</h1>
+                <Progessbar/>
+    
+                <h1>Задачи</h1>
 
             </div>
-            <h1 className="red">Main</h1>
-            <Progessbar/>
         </div>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import './Navbar.css'
 
 import Icons from '../static/Icons';
+import Net from '../utils/Net';
 
 export default function Navbar() {
-
-    const score = 102;
+    const score = Net.get('score', "100");
 
     return (
         <div className='navbar-box'>
-            <div className='score-box'>{score}{Icons.score}</div>
+            <div className='navbar-size'>
+                <div className='score-box'>{score}{Icons.score}</div>
+            </div>
         </div>
     );
 }
