9744ed5b1ea6e9c6ad49a9d421cc25eb5abd4e34
TempUser <temp@mail>
2025-05-31 10:03:47 +0300

    Added homepage

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Main</h1>
+    </div>
+  );
+}
+
+
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <div>404</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+    // 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
