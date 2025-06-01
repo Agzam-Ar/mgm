24c6003cd7e65eefe98dba8263529b1b3232ead1
TempUser <temp@mail>
2025-06-01 09:00:36 +0300

    Добавлен хук для счета

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,20 @@
 
+import React from "react";
 import Progessbar from "./components/base/Progressbar";
 import Checklist from "./components/checklist/Checklist";
 import Navbar from "./components/Navbar";
+import useScore from "./hooks/useScore";
 
 export default function Home({edit=false}:{edit?:boolean}) {
+
+    const [score, setScore] = useScore('home-page');
+
     return (
         <div className="body">
             <Navbar/>
             <div className="page-box">
                 <h1>Цель</h1>
-                <Progessbar/>
+                <Progessbar value={score}/>
                 <h1>Задачи</h1>
                 <Checklist edit={edit}/>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,23 @@ import Icons from '../static/Icons';
 import Net from '../utils/Net';
 import React from 'react';
 import Vars from '../static/Vars';
+import useScore from '../hooks/useScore';
 
 export default function Navbar() {
-    const [score, setScore] = React.useState<number>(parseInt(Net.get('score', "" + Vars.score)));
+    const [score, setScore] = useScore('navbar');//React.useState<number>(parseInt(Net.get('score', "" + Vars.score)));
 
-    React.useEffect(() => {
-        Net.onJson('set-score', (e:any) => {
-            Vars.score = e.score;
-            setScore(e.score);
-        });
-    }, []);
+    // React.useEffect(() => {
+    //     Net.onJson('set-score', (e:any) => {
+    //         Vars.score = e.score;
+    //         Net.set('score', Vars.score+"");
+    //         setScore(e.score);
+    //     });
+    // }, []);
 
     return (
         <div className='navbar-box'>
             <div className='navbar-size'>
-                <div className='score-box'>{score}{Icons.score}</div>
+                <div className='score-box'>{score+""}{Icons.score}</div>
             </div>
         </div>
     );
